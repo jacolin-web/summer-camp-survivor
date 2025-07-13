120878f867bfea9ad1b34d1799eefbaacad87a24
Jacqueline Linang <Jacque3L@verizon.net>
2025-07-12 23:53:54 -0400

    Added win game logic and clean up

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,7 @@ import { PhaserGame } from './PhaserGame';
 
 function App ()
 {
-    const phaserRef = useRef();
-    const [started, setStarted] = useState(false);
-
-    const startGame = () => {
-    const mainMenu = phaserRef.current?.scene; // this is MainMenu scene
-
-    if (mainMenu && mainMenu.scene.key === 'MainMenu') {
-      mainMenu.scene.launch('DialogueScene', {
-        lines: [
-          'Welcome to the forest.',
-          'Avoid the zombie if you can.',
-          'Click to move around!'
-        ],
-        onComplete: () => {
-          mainMenu.scene.start('Game'); // start game AFTER dialogue
-          setStarted(true);             // hide start button
-        }
-      });
-      setStarted(true);
-    }
-  };
+  const phaserRef = useRef();
 
   return (
     <div id="app">
